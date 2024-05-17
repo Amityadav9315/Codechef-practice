10bf1417770bb0d1fd10be9413fc877c6c2caf67
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-05-17 13:18:58 +0530

    Fitness

---
+++ src/BasicMath/Fitness.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Fitness {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int t = scanner.nextInt();
+
+            while (t-- > 0) {
+                int x = scanner.nextInt();
+                System.out.println(x*10);
+            }
+
+            // Your code goes here
+        }
+    }
+
+
