cc6800fc1376c9979edb2ac8241c8841838597d8
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-05-23 11:36:29 +0530

    Table

---
+++ src/BasicMath/Table.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Table{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number ");
+        int n=sc.nextInt();
+        for(int i=1; i<=10; i++){
+            System.out.println(n+"*"+i+"="+n*i);
+        }
+
+    }
+}
