77b506dab584bf17f9163b727f4026457a82cb59
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-05-24 19:26:32 +0530

    Psver

---
+++ src/BasicMath/Psver.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Psver
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int []arr=new int[n];
+            for(int i=0; i<n; i++){
+                arr[i]=sc.nextInt();
+
+            }
+            int count=0;
+            for(int i=0; i<arr.length; i++){
+                if(arr[i]>4){
+                    count++;
+                }
+
+
+
+
+            }
+            if(count==n){
+                System.out.println("Yes");
+            }
+            else{
+                System.out.println("No");
+            }
+        }
+        // your code goes here
+
+    }
+}
