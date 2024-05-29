abb55a15a98f9d46e26ff1379c3fcbf59b48d22d
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-05-29 23:51:14 +0530

    HackerMan

---
+++ src/BasicMath/HackerMan.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+
+class HackerMan
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Entre the no of test case");
+
+        int t=sc.nextInt();
+
+        while(t-->0){
+            int count=0;
+            System.out.println("Enter the value of a");
+            int a=sc.nextInt();
+            System.out.println("Enter the value of b");
+            int b=sc.nextInt();
+            int c=a+b;
+            for(int i=2; i<c; i++){
+                if(c%i==0){
+                    System.out.println("Not prime no");
+                    count=1;
+                    break;
+                }
+            }
+            if(count==0){
+                System.out.println("Prime no");
+            }
+
+        }
+        // your code goes here
+
+    }
+}
