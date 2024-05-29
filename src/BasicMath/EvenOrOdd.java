1b4cda43802c52b9f330351cc1fbef369348fdc7
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-05-30 00:04:05 +0530

    EvenOdd

---
+++ src/BasicMath/EvenOrOdd.java
@@ -0,0 +1,16 @@
+import java.util.*;
+        class EvenOrOdd{
+            public static void main(String[] args) {
+                Scanner sc=new Scanner(System.in);
+                System.out.println("Entre the number");
+                int n=sc.nextInt();
+                if(n%2==0){
+                    System.out.println("Even number");
+
+                }
+                else{
+                    System.out.println("Odd number");
+                }
+
+            }
+        }
