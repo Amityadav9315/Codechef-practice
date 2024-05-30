aebc8726eaffa5a08e0ad6b771b2c7e23ec8d6a6
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-05-30 09:46:39 +0530

    BlackJack

---
+++ src/BasicMath/BlackJack.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class BlackJack
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Entre the number of test case");
+        int t=sc.nextInt();
+        while(t-->0){
+            System.out.println("Entre the value of A");
+            int a=sc.nextInt();
+            System.out.println("Enter the value of B");
+            int b=sc.nextInt();
+            int c=21-(a+b);
+            if(c>=1&&c<=10){
+                System.out.println(c);
+            }
+            else{
+                System.out.println("-1");
+            }
+        }
+
+        // your code goes here
+
+    }
+}
