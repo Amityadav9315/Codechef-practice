0e7803c62137c999d838857f68dd6b74bcf4d07d
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-06-01 12:58:28 +0530

    OnlineOrOffline

---
+++ src/BasicMath/OnlineOrOffline.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class OnlineOrOffline
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc=new Scanner(System.in);
+
+        {
+            int n=sc.nextInt();
+            int m=sc.nextInt();
+            double dis=n-(n*0.1);
+            if(m==dis)
+            {
+                System.out.println("EITHER");
+            }
+            else if(m<dis){
+                System.out.println("DINING");
+            }
+            else{
+                System.out.println("ONLINE");
+            }
+        }
+    }
+}
