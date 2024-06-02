819a33dcf36fc0376c0e0dfe13e497eabbddda3b
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-06-02 11:23:19 +0530

    SurvivalTime

---
+++ src/BasicMath/SurvivalTime.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class SurvivalTime
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            int x=sc.nextInt();
+            int d=sc.nextInt();
+            int c=n/(5*x);
+            System.out.println(c+d);
+
+        }
+        // your code goes here
+
+    }
+
