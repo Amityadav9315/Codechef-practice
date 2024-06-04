b012d84871376e30b88bae8b7ca9344316116a72
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-06-04 13:08:38 +0530

    Increment and decrement

---
+++ src/BasicMath/IncrementAndDecrement.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class IncrementAndDecrement {
+
+        public static void main (String[] args) throws java.lang.Exception
+        {
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            if(n%4==0){
+                System.out.println(n+1);
+                // your code goes here
+
+            }
+            else{
+                System.out.println(n-1);
+            }
+        }
+    }
+
+
