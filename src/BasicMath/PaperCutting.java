9d97d6ab5083d913d83819bdf1583ffd559c12e8
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-06-06 13:22:34 +0530

    PaperCutting

---
+++ src/BasicMath/PaperCutting.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class PaperCutting {
+        public static void main (String[] args) throws java.lang.Exception {
+            Scanner sc = new Scanner(System.in);
+                int n = sc.nextInt();
+                int k = sc.nextInt();
+                int d = n / k;
+
+                System.out.println(d*d);
+                //}
+                //  else{
+                //  System.out.println((d/e)-k);
+
+
+                // your code goes here
+
+            }
+
+
+        }
