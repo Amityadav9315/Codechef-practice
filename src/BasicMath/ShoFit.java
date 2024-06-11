97471b371b0d3651e5d0f5a210c33e7e7ea36585
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-06-12 03:36:27 +0530

    ShoFit

---
+++ src/BasicMath/ShoFit.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class ShoFit {
+        public static void main (String[] args) throws java.lang.Exception
+        {
+            Scanner sc=new Scanner(System.in);
+                int a=sc.nextInt();
+                int b=sc.nextInt();
+                int c=sc.nextInt();
+                if(a==0&&b==0&&c==0||a==1&&b==1&&c==1){
+                    System.out.println("0");
+                }
+                else{
+                    System.out.println("1");
+                }
+            }
+            // your code goes here
+
+        }
+
+
+
