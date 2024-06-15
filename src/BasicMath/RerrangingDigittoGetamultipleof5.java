0643ce90c9192d31e7233827385ddd64b87a3044
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-06-15 20:25:31 +0530

    RerrangingDigit

--- src/BasicMath/RerrangingDigittoGetamultipleof5.java
+++ src/BasicMath/RerrangingDigittoGetamultipleof5.java
@@ -3,16 +3,18 @@ public class RerrangingDigittoGetamultipleof5 {
         public static void main (String[] args) throws java.lang.Exception
         {
             Scanner sc=new Scanner(System.in);
-                i nt count=0;
+                int count=0;
                 int n=sc.nextInt();
                 int[] arr=new  int[n];
+                for(int i=0; i<n; i++) {
+                    arr[i] = sc.nextInt();
+                }
                 for(int i=0; i<n; i++){
-                    arr[i]=sc.nextInt();
-                    if(arr[i]%5==0){
+                    if(arr[i]==0||arr[i]==5){
                         count++;
                     }
                 }
-                if(count<=1){
+                if(count>=1){
                     System.out.println("Yes");
 
                 }
