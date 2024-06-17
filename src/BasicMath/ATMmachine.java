3e57d26b24ccc98f0eda7f24da2b39f3eaed9a4c
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-06-17 17:15:52 +0530

    ATMmachie

--- src/BasicMath/ATMmachine.java
+++ src/BasicMath/ATMmachine.java
@@ -1,32 +1,34 @@
 import java.util.*;
-        public static void main (String[] args) throws java.lang.Exception
-        {
-            Scanner sc=new Scanner(System.in);
-                int n=sc.nextInt();
-                int k=sc.nextInt();
-                int[] arr=new int[n];
-                for(int i=0; i<n; i++){
-                    arr[i]=sc.nextInt();
+class ATMmachine{
+        public static void main (String[] args) throws java.lang.Exception {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
 
-                }
-                int l=k;
-                for(int i=0; i<arr.length; i++){
-                    if(arr[i]<=k){
-                        l=l-arr[i];
-                        if(l>=0)
-                        {
-                            System.out.print("1");
-                        }
-                        else{
-                            System.out.print("0");
-                            l=l+arr[i];
-                        }
+            }
+            int l = k;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] <= k) {
+                    l = l - arr[i];
+
+                    if (l >= 0) {
+                        System.out.print("1");
+
+                    } else {
+                        System.out.print("0");
+                        l = l + arr[i];
                     }
-                    else{
+                }
+                 else{
                         System.out.print("0");
                     }
                 }
                 System.out.println();
+            }
+            }
 
 
             // your codegoeshere
