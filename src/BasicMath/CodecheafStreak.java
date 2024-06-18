bdf7b71037d5db39de045875e115db82b7009298
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-06-18 11:53:51 +0530

    CodecheafSreak

--- src/BasicMath/CodecheafStreak.java
+++ src/BasicMath/CodecheafStreak.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class CodecheafStreak {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
+            System.out.println("Entre the value of n");
             int n=scanner.nextInt();
                 int count1 = 0;
                 int count2 = 0;
