b052edf8fc0d296e37815df3630ceab8f6ecab3e
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-06-19 19:11:28 +0530

    CodecheafStreak

--- src/BasicMath/CodecheafStreak.java
+++ src/BasicMath/CodecheafStreak.java
@@ -1,49 +1,57 @@
 import java.util.*;
 public class CodecheafStreak {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter the value of n");
-            int n=scanner.nextInt();
-                int count1 = 0;
-                int count2 = 0;
-                int count3 = 0;
-                int count4 = 0;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the value of n");
+        int n = scanner.nextInt();
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
+        int count4 = 0;
 
-                int[] a = new int[n];
-                int[] b = new int[n];
+        int[] a = new int[n];
+        int[] b = new int[n];
 
-                for (int i = 0; i < n; i++) {
-                    a[i] = scanner.nextInt();
-                    if (a[i] > 0) {
-                        count1++;
-                    } else if (a[i] == 0) {
-                        count2=(count2+1)-1;
-                    }
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+            if (a[i] > 0) {
+                count1++;
+            } else {
+                if (count2<count1){
+                    count2 = count1;
                 }
+                count1 = 0;
 
-                int max1 = Math.max(count1, count2);
-
-                for (int i = 0; i < n; i++) {
-                    b[i] = scanner.nextInt();
-                    if (b[i] > 0) {
-                        count3++;
-                    } else if (b[i] == 0) {
-                        count4=(count4+1)-1;
-                    }
-                }
-
-                int max2 = Math.max(count3, count4);
-
-                if (max1 == max2) {
-                    System.out.println("Draw");
-                } else if (max1 > max2) {
-                    System.out.println("Om");
-                } else {
-                    System.out.println("Addy");
-                }
             }
-
         }
+
+        int max1 = Math.max(count1, count2);
+
+        for (int i = 0; i < n; i++) {
+            b[i] = scanner.nextInt();
+            if (b[i] > 0) {
+                count3++;
+            } else {
+                if (count4 < count3) {
+                    count4 = count3;
+                }
+                count3 = 0;
+            }
+        }
+
+            int max2 = Math.max(count3, count4);
+
+            if (max1 == max2) {
+                System.out.println("Draw");
+            } else if (max1 > max2) {
+                System.out.println("Om");
+            } else {
+                System.out.println("Addy");
+            }
+        }
+
+    }
+
 
 
 
