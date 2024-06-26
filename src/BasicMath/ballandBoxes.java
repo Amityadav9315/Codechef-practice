e5106a876436cd5ebee6f460d71425bfcde1c197
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-06-26 19:09:43 +0530

    ballandBoxes

---
+++ src/BasicMath/ballandBoxes.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class ballandBoxes {
+        public static void main (String[] args) throws java.lang.Exception
+        {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the number");
+                int n=sc.nextInt();
+                int k=sc.nextInt();
+                if(n==k){
+                    System.out.println("No");
+                }
+               // else if()
+            }
+            // your code goes here
+
+        }
+
+
+
