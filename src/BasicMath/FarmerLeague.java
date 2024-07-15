a9e2953208822599e4e33e56210772d2bb01648b
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-07-15 23:08:00 +0530

    FarmerLeague

---
+++ src/BasicMath/FarmerLeague.java
@@ -0,0 +1,19 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class FarmerLeague
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner read = new Scanner(System.in);
+        int t = read.nextInt();
+        for(int i=0; i<t; i++){
+            int n = read.nextInt();
+            System.out.println((n/2)*3);
+        }
+    }
+}
