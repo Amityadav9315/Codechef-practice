f3ca337f48f3cfd79160a4c16fcd5a2b7ccec45e
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-07-16 22:05:22 +0530

    DevouringDounuts

--- src/BasicMath/DevouringDounuts.java
+++ src/BasicMath/DevouringDounuts.java
@@ -7,6 +7,7 @@ class Devouringdounuts
     public static void main (String[] args) throws java.lang.Exception
     {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
         int x=sc.nextInt();
         int y=sc.nextInt();
         System.out.println(x*y);
