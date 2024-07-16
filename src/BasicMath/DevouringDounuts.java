8531bd1e1a36e7ad1b92c103d84f528c29af3a9b
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-07-16 22:04:08 +0530

    DevouringDounuts

---
+++ src/BasicMath/DevouringDounuts.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Devouringdounuts
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        System.out.println(x*y);
+        // your code goes here
+
+    }
+}
