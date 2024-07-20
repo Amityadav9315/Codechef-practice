83b2dbbfca369a7120ff7d45ab2db3142402d491
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-07-20 22:14:23 +0530

    carTrip

---
+++ src/BasicMath/CarTrip.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class CarTrip
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            if(n<=300){
+                System.out.println("3000");
+
+            }
+            else{
+                System.out.println(n*10);
+            }
+        }
+        // your code goes here
+
+    }
+
