9aa37e4ffad0f547244429fc76150eb9cf4e1549
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-07-22 22:05:50 +0530

    MasterCheafFinal

---
+++ src/Array/MastreCheafFinal.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class MasterCheafFinal
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+            int x=sc.nextInt();
+            if(x<=10){
+                System.out.println("Yes");
+            }
+            else{
+                System.out.println("No");
+            }
+            // your code goes here
+
+        }
+    }
+
