9e8a87b4d66b728261bb589c94aec76226a2bbef
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-07-22 21:56:25 +0530

    Puppyandsum

---
+++ src/Array/PuppyandSum.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class PuppyandSum {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+
+         int sum=0;
+        int d = sc.nextInt();
+        int n = sc.nextInt();
+        if (d > 1) {
+
+            while (d > 0) {
+
+                for (int i = n; i > 0; i--) {
+                    sum = sum + i;
+
+
+                }
+                n=sum;
+                sum=0;
+                d--;
+
+            }
+        }
+            else{
+
+                for (int i = n; i > 0; i--) {
+                    sum = sum + i;
+
+
+                }
+                n=sum;
+            }
+            System.out.println(n);
+        }
+    }
+
+
+
+
