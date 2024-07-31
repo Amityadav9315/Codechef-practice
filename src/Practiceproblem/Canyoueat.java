352a87edf3188c9aeb9de21365b0b7b16cd312f4
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-07-31 14:35:44 +0530

    Canyoueat

---
+++ src/Practiceproblem/Canyoueat.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Canyoueat
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            int k=sc.nextInt();
+            int d=n%k;
+            int e=n/k;
+            if(n==0){
+                System.out.println("0");
+            }
+            else if(d==0){
+                System.out.println(e);
+            }
+            else{
+                System.out.println("-1");
+            }
+        }
+        // your code goes here
+
+    }
+
