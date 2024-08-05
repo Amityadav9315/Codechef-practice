f04085d60adcd19bf443b3b4e5e9f1dc186ac88b
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-08-05 22:16:54 +0530

    ticketfine

---
+++ src/Array/TicketFine.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class TicketFine
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+            int x=sc.nextInt();
+            int p=sc.nextInt();
+            int q=sc.nextInt();
+            int c=p-q;
+            System.out.println(c*x);
+        }
+        // your code goes here
+
+    }
+
