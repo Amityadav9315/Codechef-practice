c02a2fbbbbc0256f05275cabe8519021abe55b07
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-08-07 08:55:20 +0530

    CricketMatch

---
+++ src/Practiceproblem/CricketMatch.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class CricketMatch
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            int m=sc.nextInt();
+            int c=6*6*m;
+            if(c>=n){
+                System.out.println("Yes");
+
+            }
+            else{
+                System.out.println("No");
+            }
+        }
+        // your code goes here
+
+    }
+
