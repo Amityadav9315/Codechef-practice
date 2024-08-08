b3b0af59a90367f934873d26722e5f87a24242df
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-08-08 23:26:37 +0530

    Marathon

---
+++ src/Array/Marathon.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Marathon
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int D=sc.nextInt();
+            int d=sc.nextInt();
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+
+            int maxdist=D*d;
+            if(maxdist<10){
+                System.out.println("0");
+            }
+            else if(maxdist>=10&&maxdist<21){
+                System.out.println("1");
+            }
+            else if(maxdist>=21&&maxdist<42){
+                System.out.println("2");
+            }
+            else if(maxdist>=42){
+                System.out.println("3");
+            }
+            // your code goes here
+
+        }
+    }
+}
