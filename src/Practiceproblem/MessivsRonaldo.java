58fecf4e3e8827e420c005bf30f3bf12e2fa2e3b
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-08-10 12:07:33 +0530

    MessivsRonaldo

---
+++ src/Practiceproblem/MessivsRonaldo.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class MessivsRonaldo
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int c=a*2+b;
+        int f=x*2+y;
+        if(c>f){
+            System.out.println("Messi");
+        }
+        else if(c==f){
+            System.out.println("Equal");
+        }
+        else{
+            System.out.println("Ronaldo");
+        }
+        // your code goes here
+
+    }
+}
