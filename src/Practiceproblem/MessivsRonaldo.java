d6841a7766b8ae07bdfca719553517f60f3f2d70
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-08-10 12:08:52 +0530

    MessivsRonaldo

--- src/Practiceproblem/MessivsRonaldo.java
+++ src/Practiceproblem/MessivsRonaldo.java
@@ -7,6 +7,7 @@ class MessivsRonaldo
     public static void main (String[] args) throws java.lang.Exception
     {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Entre the number");
         int a=sc.nextInt();
         int b=sc.nextInt();
         int x=sc.nextInt();
