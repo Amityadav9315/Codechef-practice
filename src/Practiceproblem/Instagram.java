cbc2f77874cbcec35eb5441775737690a33a9ff8
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-08-18 00:45:57 +0530

    Instagram

---
+++ src/Practiceproblem/Instagram.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class    Instagram
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            int c=x*10;
+            if(c>10){
+                System.out.println("Yes");
+
+            }
+            else{
+                System.out.println("N0");
+            }
+        }
+        // your code goes here
+
+    }
+}
