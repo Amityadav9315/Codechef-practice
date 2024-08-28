9811d51650b51338b03b48cb839d6828f1c7734a
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-08-28 20:47:44 +0530

    AirlineRestriction

---
+++ src/Array/AirlineRestriction.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class AirlineRestriction
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            int d=sc.nextInt();
+            int e=sc.nextInt();
+            if((a+b<=d) &&c<=e){
+                System.out.println("Yes");
+
+            }
+            else if(a+c<=d&&b<=e){
+                System.out.println("Yes");
+            }
+            else{
+                System.out.println("No");
+            }
+
+
+        }
+
+        // your code goes here
+
+    }
+}
