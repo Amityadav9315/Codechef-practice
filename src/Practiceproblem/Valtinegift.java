797104dafb271ef95b944490ff07791b584fc280
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-08-30 12:46:21 +0530

    Valtinegift

---
+++ src/Practiceproblem/Valtinegift.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Valtinegift
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            if(n>=127){
+                System.out.println("Yes");
+            }
+            else{
+                System.out.println("No");
+            }
+        }
+        // your code goes here
+
+    }
+}
