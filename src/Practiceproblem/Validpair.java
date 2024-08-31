6ca5be17edf7c9a752120fdd42c11db4bd18cf50
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-08-31 11:28:32 +0530

    Validpair

---
+++ src/Practiceproblem/Validpair.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class validpair.
+        git
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        if(a==b||b==c||c==a){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+        // your code goes here
+
+    }
+}
