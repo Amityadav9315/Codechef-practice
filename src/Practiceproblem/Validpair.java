8fb5e7d642703f6cf5159c840d409f48b7a14cdc
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-08-31 11:30:10 +0530

    Validpair

--- src/Practiceproblem/Validpair.java
+++ src/Practiceproblem/Validpair.java
@@ -2,12 +2,13 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class validpair.
-        git
+class Validpair
+
 {
     public static void main (String[] args) throws java.lang.Exception
     {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
         int a=sc.nextInt();
         int b=sc.nextInt();
         int c=sc.nextInt();
