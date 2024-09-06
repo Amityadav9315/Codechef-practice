67dc06ba334c38452dcd665d899dd8fd9735a65e
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-09-06 14:41:30 +0530

    friendMeetup

---
+++ src/String/FriendMeetup.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class FriendMeetup
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int x1=sc.nextInt();
+            int x2=sc.nextInt();
+
+
+            if(x1==x2||x1>x2){
+                System.out.println("Yes");
+            }
+
+            else{
+                System.out.println("No");
+            }
+        }
+    }
+    // your code goes here
+
+}
+
