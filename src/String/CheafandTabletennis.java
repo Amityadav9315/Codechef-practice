3b5f3acb1c0089610d5411675dc614c6ea13a6bb
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-09-07 16:53:20 +0530

    CheafandTabletennis

---
+++ src/String/CheafandTabletennis.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class CheafandTabletennis
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+
+        while(t-->0){
+            int count=0,count1=0;
+            String str=sc.next();
+            for(int i=0; i<str.length(); i++){
+                if(str.charAt(i)=='1'){
+                    count++;
+                }
+                else{
+                    count1++;
+                }
+            }
+            if(count>count1){
+                System.out.println("WIN");
+            }
+            else{
+                System.out.println("LOSE");
+
+            }
+
+            // your code goes here
+
+        }
+    }
+}
