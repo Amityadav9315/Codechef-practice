f884e36b7c8857a2e99cdeacc856982793ce0f99
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-09-11 10:59:52 +0530

    WhichMixture

---
+++ src/Practiceproblem/WhichMixture.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class WhichMixture
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            if(a>0&&b>0){
+                System.out.println("Solution");
+            }
+            else if(b==0){
+                System.out.println("Solid");
+
+            }
+            else{
+                System.out.println("Liquid");
+            }
+            // your code goes here
+
+        }
+    }
+}
