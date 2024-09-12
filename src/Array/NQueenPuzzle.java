af209cefa2a0d41e2aeb466c750c368a7d2af120
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-09-12 14:45:13 +0530

    NQueenPuzzle

---
+++ src/Array/NQueenPuzzle.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class NQueenPuzzle
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+        int d=0;
+        int count=0;
+
+
+            int n=sc.nextInt();
+                double c=(0.143*n);
+              double l=  Math.pow(c,n);
+
+
+
+
+            d=(int) l;
+            double k=l-d;
+            if(k<0.5){
+                System.out.println(d);
+            }
+            else {
+                System.out.println(d + 1);
+            }
+
+        }
+        // your code goes here
+
+    }
+
