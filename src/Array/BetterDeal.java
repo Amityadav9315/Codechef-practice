672c6323d04748fdd9ccf09cb5ca32c8812c5ef2
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-09-14 16:24:03 +0530

    BetterDeal

---
+++ src/Array/BetterDeal.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class BetterDeal
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int d=(100-((a%100)*100));
+            int e=(200-((b%it 100)*200));
+            if(d<e){
+                System.out.println("First");
+            }
+            else if(d==e){
+                System.out.println("Both");
+            }
+            else{
+                System.out.println("Second");
+            }
+        }
+        // your code goes here
+
+    }
+
