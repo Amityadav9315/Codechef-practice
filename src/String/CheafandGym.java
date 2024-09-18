36229218fbe17fe8789f42249ac733c66a175e8b
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-09-18 20:53:51 +0530

    CheafandGym

--- src/String/CheafandGym.java
+++ src/String/CheafandGym.java
@@ -7,8 +7,6 @@ class CheafandGym
     public static void main (String[] args) throws java.lang.Exception
     {
         Scanner sc=new Scanner(System.in);
-        int t=sc.nextInt();
-        while(t-->0){
             int x=sc.nextInt();
             int y=sc.nextInt();
             int z=sc.nextInt();
@@ -27,4 +25,4 @@ class CheafandGym
         // your code goes here
 
     }
-}
+
