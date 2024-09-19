1f97430714a31a6f56926ab3e84295e679dce4a0
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-09-19 16:15:02 +0530

    CheafandContest

---
+++ src/Array/CheafandContest.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class CheafandContest
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            int p=sc.nextInt();
+            int q=sc.nextInt();
+            int c=x+10*p;
+            int d=y+10*q;
+            if(c<d){
+                System.out.println("Chef");
+            }
+            else if(c>d){
+                System.out.println("Chefina");
+            }
+            else{
+                System.out.println("Draw");
+            }
+        }
+        // your code goes here
+
+    }
+}
