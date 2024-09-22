c583b7b477b9177bfd4e5077a77051b880af161e
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-09-22 16:52:50 +0530

    GoodWeather

--- src/Array/MovieWeakend.java
+++ src/Array/MovieWeakend.java
@@ -7,6 +7,7 @@ class MovieWeakend
     public static void main (String[] args) throws java.lang.Exception
     {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
         int t=sc.nextInt();
         while(t-->0){
             int n=sc.nextInt();
@@ -14,10 +15,14 @@ class MovieWeakend
             for(int i=0; i<n; i++){
                 arr[i]=sc.nextInt();
             }
-            int[] arr=new int[n];
-            for(int i=0; i<n; i++){
-                arr[i]=sc.nextInt();
+            int[] arr1=new int[n];
+            for(int j=0; j<n; j++){
+                arr[j]=sc.nextInt();
             }
+            for(int k=0; k<n; k++){
+                int c=arr[i]*arr1[j];
+            }
+
 
 
 
