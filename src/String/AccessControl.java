e29a66eacb68cdf8f88158fd85b2b39957332a79
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-09-25 19:20:13 +0530

    AccessControl

---
+++ src/String/AccessControl.java
@@ -0,0 +1,22 @@
+package String;
+import java.util.*;
+
+public class AccessControl {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int x=sc.nextInt();
+        int count=0;
+        int count1=0;
+
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int j=0; j<n; j++){
+
+        }
+
+
+    }
+}
