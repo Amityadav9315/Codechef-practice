3763a20b12835b5cb3e704d1ca69c3390977b429
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-09-29 09:09:29 +0530

    StringGame

---
+++ src/String/StringGame.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class StringGame
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+
+            int n=sc.nextInt();
+            String str=sc.next();
+        System.out.println(str);
+          //  for(int i=0; i<n; i++){
+
+            }
+        }
+        // your code goes here
+
+
+
